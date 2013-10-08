3ab15bcf0dbdc3a8a679a07d6f3aaaa64f3c74a2
jmrgonza <jmrgonza@gmail.com>
2013-10-08 15:37:36 +0100

    Primera versión de sum (correcta) y de sum_2_max (no funciona), con tests correspondientes.

--- HW0/hw_0_1.rb
+++ HW0/hw_0_1.rb
@@ -1,9 +1,33 @@
 def sum(lista)
-  
+  total = 0
+  lista.each{ |elem| total += elem }
+  total
 end
 
 def max_2_sum(lista)
+  l = lista.length
+  a = l>0 ? lista[0] : 0
+  b = l>1 ? lista[1] : 0
+
+  if (b>a)
+    c = b
+    b = a
+    a = c
+  end
   
+  lista.each  do |elem|
+    if (elem>b)
+      b = elem
+    end
+    if (b>a)
+      c = b
+      b = a
+      a = c
+    end
+  end
+
+  a + b
+
 end
 
 def sum_to_n?(lista, target)
---
+++ HW0/test.rb
@@ -0,0 +1,28 @@
+require "test/unit"
+require "hw_0_1.rb"
+
+class TestHW < Test::Unit::TestCase
+  def test_sum
+    assert_equal(0, sum([]))
+    assert_equal(0, sum([0]))
+    assert_equal(5, sum([5]))
+    assert_equal(3, sum([1,1,1,0]))
+    assert_equal(6, sum([1,2,3]))
+    assert_equal(10, sum([1,2,3,4]))
+  end
+  
+  def test_max_2_sum
+    assert_equal(0, max_2_sum([]))
+    assert_equal(0, max_2_sum([0]))
+    assert_equal(1, max_2_sum([1]))
+    assert_equal(-1, max_2_sum([-1]))
+    assert_equal(6, max_2_sum([6]))  
+    assert_equal(0, max_2_sum([0,0]))
+    assert_equal(1, max_2_sum([0,1]))
+    assert_equal(0, max_2_sum([-1,1]))
+    assert_equal(-3, max_2_sum([-1,-2]))
+    assert_equal(5, max_2_sum([3,2]))
+    assert_equal(6, max_2_sum([1,2,3]))
+  end
+end
+
