0340e36d77f7331d8cd6e3104f6d468121afb6a7
jmrgonza <jmrgonza@gmail.com>
2013-10-10 09:42:03 +0100

    Títulos de los ejercicios

--- HW0/hw_0_1.rb
+++ HW0/hw_0_1.rb
@@ -1,10 +1,16 @@
 def sum(lista)
+  # Define a method sum which takes an array of integers as an argument
+  # and returns the sum of its elements. For an empty array it should return zero.
   total = 0
   lista.each{ |elem| total += elem }
   total
 end
 
 def max_2_sum(lista)
+  # Define a method max_2_sum which takes an array of integers as an argument
+  # and returns the sum of its two largest elements.
+  # For an empty array it should return zero.
+  # For an array with just one element, it should return that element.
   n = lista.length
   if (n==0)
     return 0
@@ -26,7 +32,9 @@ def max_2_sum(lista)
 end
 
 def sum_to_n?(lista, target)
-  
+  # Define a method sum_to_n? which takes an array of integers and an additional integer, n,
+  # as arguments and returns true if any two elements in the array of integers sum to n.
+  # An empty array should sum to zero by definition.
   if lista.length==0
     return (target==0)
   elsif lista.length==1
