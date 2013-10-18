327d67c0888c74760d2c0ff4796a240476f444b2
jmrgonza <jmrgonza@gmail.com>
2013-10-18 13:11:11 +0100

    Entregada la tercera parte de HW1 (piedra, papel, tijeras)

--- HW1/public/lib/rock_paper_scissors.rb
+++ HW1/public/lib/rock_paper_scissors.rb
@@ -4,11 +4,28 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    strategy1 = player1[1].upcase
+    strategy2 = player2[1].upcase
+   
+    raise self::NoSuchStrategyError, "Strategy must be one of R,P,S" unless (['R','S','P'].include?(strategy1) && ['R','S','P'].include?(strategy2))  
+
+    if (strategy1=='P') && (strategy2=='S')
+      return player2
+    elsif (strategy1=='S') && (strategy2=='R')
+      return player2
+    elsif (strategy1=='R') && (strategy2=='P')
+      return player2
+    else
+      return player1
+    end
   end
 
   def self.tournament_winner(tournament)
-    # YOUR CODE HERE
+    if tournament[0].is_a?(String)
+      return tournament
+    else
+      return self.winner(self.tournament_winner(tournament[0]), self.tournament_winner(tournament[1]))
+    end
   end
 
 end
