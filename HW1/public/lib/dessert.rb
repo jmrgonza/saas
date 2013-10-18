bbf0d1294c60c2017c302a102704b341371e249a
jmrgonza <jmrgonza@gmail.com>
2013-10-18 11:59:25 +0100

    Entregada la segunda parte de la práctica HW1

--- HW1/public/lib/dessert.rb
+++ HW1/public/lib/dessert.rb
@@ -1,17 +1,41 @@
 class Dessert
   def initialize(name, calories)
-    # your code here
+    @name = name
+    @calories = calories
+  end
+  def name
+    @name
+  end
+  def name=(other)
+    @name=other
+  end
+  def calories
+    @calories
+  end
+  def calories=(other)
+    @calories=other
   end
   def healthy?
-    # your code here
+    @calories < 200
   end
   def delicious?
-    # your code here
+    true
   end
 end
 
 class JellyBean < Dessert
   def initialize(flavor)
-    # your code here
+    @flavor=flavor
+    super(@flavor + " jelly bean", 5)
+  end
+  def flavor
+    @flavor
+  end
+  def flavor=(other)
+    @flavor=other
+    @name=@flavor + " jelly bean"
+  end
+  def delicious?
+    @flavor!="licorice"
   end
 end
